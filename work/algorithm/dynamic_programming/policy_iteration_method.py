bf64164ce95131e686230178c384265259d30bd8
Bin.Li <ornot2008@yahoo.com>
2020-02-11 09:29:13 +0800

    add docstring

--- work/algorithm/dynamic_programming/policy_iteration_method.py
+++ work/algorithm/dynamic_programming/policy_iteration_method.py
@@ -1,9 +1,50 @@
+# #### BEGIN LICENSE BLOCK #####
+# Version: MPL 1.1/GPL 2.0/LGPL 2.1
+#
+# The contents of this file are subject to the Mozilla Public License Version
+# 1.1 (the "License"); you may not use this file except in compliance with
+# the License. You may obtain a copy of the License at
+# http://www.mozilla.org/MPL/
+#
+# Software distributed under the License is distributed on an "AS IS" basis,
+# WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+# for the specific language governing rights and limitations under the
+# License.
+#
+#
+# Contributor(s):
+#
+#    Bin.Li (ornot2008@yahoo.com)
+#
+#
+# Alternatively, the contents of this file may be used under the terms of
+# either the GNU General Public License Version 2 or later (the "GPL"), or
+# the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
+# in which case the provisions of the GPL or the LGPL are applicable instead
+# of those above. If you wish to allow use of your version of this file only
+# under the terms of either the GPL or the LGPL, and not to allow others to
+# use your version of this file under the terms of the MPL, indicate your
+# decision by deleting the provisions above and replace them with the notice
+# and other provisions required by the GPL or the LGPL. If you do not delete
+# the provisions above, a recipient may use your version of this file under
+# the terms of any one of the MPL, the GPL or the LGPL.
+#
+# #### END LICENSE BLOCK #####
+#
+# /
+
+
 import copy
 
 from lib.utility import create_distribution_greedily
 
 
 class Policy_Iteration_Method:
+    """
+     Once a ploicy has been improved to get a new sequence of value of states, we can
+     then compute the value of the states to improve the pollicy, monotonically. 
+    """
+
     def __init__(self, v_table, policy, transition_table, delta=1e-5, discount=1.0):
         self.v_table = v_table
         self.policy = policy
@@ -17,7 +58,6 @@ class Policy_Iteration_Method:
             if delta < self.delta:
                 break
 
-   
     def evaluate_once(self):
         delta = 1e-10
         for state_index, old_value_of_state in self.v_table.items():
@@ -25,7 +65,8 @@ class Policy_Iteration_Method:
             action_transitions = self.transition_table[state_index]
             for action_index, transitions in action_transitions.items():
                 value_of_action = self._get_value_of_action(transitions)
-                value_of_state += self.policy.policy_table[state_index][action_index] * value_of_action
+                value_of_state += self.policy.policy_table[state_index][action_index] * \
+                    value_of_action
             self.v_table[state_index] = value_of_state
             delta = max(abs(value_of_state-old_value_of_state), delta)
         return delta
@@ -40,15 +81,16 @@ class Policy_Iteration_Method:
                 q_values[action_index] = self._get_value_of_action(transition)
             greedy_distibution = create_distribution_greedily()(q_values)
             self.policy.policy_table[state_index] = greedy_distibution
-            new_old_policy_diff = {action_index:abs(old_policy[action_index]-greedy_distibution[action_index])                 for action_index in greedy_distibution}   
+            new_old_policy_diff = {action_index: abs(
+                old_policy[action_index]-greedy_distibution[action_index]) for action_index in greedy_distibution}
             delta = max(max(new_old_policy_diff.values()), delta)
         return delta
-    
 
     def _get_value_of_action(self, transitions):
         value_of_action = 0.0
         for transition_prob, next_state_index, reward, done in transitions:  # For each next state
             # the reward is also related to the next state
             value_of_next_state = 0 if done else self.v_table[next_state_index]
-            value_of_action += transition_prob * (self.discount*value_of_next_state+reward)
+            value_of_action += transition_prob * \
+                (self.discount*value_of_next_state+reward)
         return value_of_action
--- work/algorithm/dynamic_programming/v_value_iteration_method.py
+++ work/algorithm/dynamic_programming/v_value_iteration_method.py
@@ -1,9 +1,61 @@
-import copy
+# #### BEGIN LICENSE BLOCK #####
+# Version: MPL 1.1/GPL 2.0/LGPL 2.1
+#
+# The contents of this file are subject to the Mozilla Public License Version
+# 1.1 (the "License"); you may not use this file except in compliance with
+# the License. You may obtain a copy of the License at
+# http://www.mozilla.org/MPL/
+#
+# Software distributed under the License is distributed on an "AS IS" basis,
+# WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+# for the specific language governing rights and limitations under the
+# License.
+#
+#
+# Contributor(s):
+#
+#    Bin.Li (ornot2008@yahoo.com)
+#
+#
+# Alternatively, the contents of this file may be used under the terms of
+# either the GNU General Public License Version 2 or later (the "GPL"), or
+# the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
+# in which case the provisions of the GPL or the LGPL are applicable instead
+# of those above. If you wish to allow use of your version of this file only
+# under the terms of either the GPL or the LGPL, and not to allow others to
+# use your version of this file under the terms of the MPL, indicate your
+# decision by deleting the provisions above and replace them with the notice
+# and other provisions required by the GPL or the LGPL. If you do not delete
+# the provisions above, a recipient may use your version of this file under
+# the terms of any one of the MPL, the GPL or the LGPL.
+#
+# #### END LICENSE BLOCK #####
+#
+# /
 
 from lib.utility import create_distribution_greedily
 
 
 class V_Value_Iteration_Method:
+    
+    """
+    One drawback to policy iteration method is that each of its iterations involves policy 
+    evalution, which may itself be a protracted iterative computation requiring multiple 
+    sweeps through the state set. If policy evaluation is done iteratively, then convergence 
+    exactly occurs in the limit.   
+    
+    In fact,the plilcy evaluation step of policy iteration can be truncated in serveral ways
+    without losing the convergence guarantees of policy iteration. One important special case
+    is when policy evaluation is stopped after just one sweep(one update of each state). This
+    alrgorithm is so called Value Iteration. 
+
+
+    Refer to the equation 4.2 in Sutton's book 
+
+
+    """
+    
+    
     def __init__(self, table_policy, v_table, p, delta=1e-8, discount=1.0):
         self.v_table = v_table
         self.policy = table_policy
