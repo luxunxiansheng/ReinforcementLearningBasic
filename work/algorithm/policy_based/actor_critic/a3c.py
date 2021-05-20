875430aea02ce4a95aaf08c8f00e92771a3ff3cf
macornot <ornot2008@yahoo.com>
2021-05-21 07:56:53 +0800

    a3c single process works

--- work/algorithm/policy_based/actor_critic/a3c.py
+++ work/algorithm/policy_based/actor_critic/a3c.py
@@ -35,78 +35,228 @@
 
 # http://mcneela.github.io/machine_learning/2019/09/03/Writing-Your-Own-Optimizers-In-Pytorch.html
 
-
-import math
-
 import torch
-import torch.optim as optim
+import torch.nn as nn 
+import torch.nn.functional as F
+
+from algorithm.policy_based.actor_critic.actor_critic_common import ValueEstimator
+from lib.utility import SharedAdam
+from common import ActorBase,CriticBase
 
 
-class SharedAdam(optim.Adam):
-    """Implements Adam algorithm with shared states.
-    """
-    def __init__(self,
-                params,
-                lr=1e-3,
-                betas=(0.9, 0.999),
-                eps=1e-8,
-                weight_decay=0):
-        super(SharedAdam, self).__init__(params, lr, betas, eps, weight_decay)
+class ValueModel(nn.Module):
+    def __init__(self,observation_space_size):
+        super().__init__()
+        self.affine = nn.Linear(observation_space_size, 128)
+        self.dropout = nn.Dropout(p=0.6)
+        self.value_head = nn.Linear(128, 1)
 
-        for group in self.param_groups:
-            for p in group['params']:
-                state = self.state[p]
-                state['step'] = torch.zeros(1)
-                state['exp_avg'] = p.data.new().resize_as_(p.data).zero_()
-                state['exp_avg_sq'] = p.data.new().resize_as_(p.data).zero_()
+    def forward(self, state):
+        state = torch.from_numpy(state).float()
+        state = F.relu(self.dropout(self.affine(state)))
+        state_value = self.value_head(state)
+        return state_value
 
-    def share_memory(self):
-        for group in self.param_groups:
-            for p in group['params']:
-                state = self.state[p]
-                state['step'].share_memory_()
-                state['exp_avg'].share_memory_()
-                state['exp_avg_sq'].share_memory_()
+class GlobalValueEestimator(ValueEstimator):
+    def __init__(self,observation_space_size):
+        self.model = ValueModel(observation_space_size)
+        self.optimizer = SharedAdam(self.model.parameters(),lr=1e-3)
 
-    def step(self, closure=None):
-        """Performs a single optimization step.
-        Arguments:
-            closure (callable, optional): A closure that reevaluates the model
-                and returns the loss.
-        """
-        loss = None
-        if closure is not None:
-            loss = closure()
+    def _sync_local_grads(local_model, global_model):
+        for local_param, global_param in zip(local_model.parameters(),global_model.parameters()):
+            if global_param.grad is not None:
+                return
+            global_param._grad = local_param.grad
 
-        for group in self.param_groups:
-            for p in group['params']:
-                if p.grad is None:
-                    continue
-                grad = p.grad.data
-                state = self.state[p]
+    def predict(self,state):
+        value = self.model.forward(state)
+        return value
 
-                exp_avg, exp_avg_sq = state['exp_avg'], state['exp_avg_sq']
-                beta1, beta2 = group['betas']
+    def update(self,*args):
+        loss = args[0]
+        local_model = args[1]
+        
+        self._sync_local_grads(self.model,local_model)
+        self.optimizer.zero_grad()
+        loss.backward()
+        self.optimizer.step()
 
-                state['step'] += 1
+class LocalValueEestimator(ValueEstimator):
+    def __init__(self,observation_space_size):
+        self.model = ValueModel(observation_space_size) 
+    
+    def predict(self,state):
+        value = self.model.forward(state)
+        return value
+    
+    def update(self, *args):
+        global_model = args[0]
+        self.model.load_state_dict(global_model.state_dict())
+        
+class OnlineCritic(CriticBase):
+    def __init__(self,global_value_estimator,discount=1.0):
+        self.global_estimator = global_value_estimator
+        self.local_esitimator = LocalValueEestimator()
+        self.discount  = discount
+    
+    def evaluate(self,*args):
+        current_state_index = args[0]
+        reward = args[1]
+        next_state_index = args[2]    
+        
+        self.local_esitimator.update(self.global_estimator.model)
+        value_of_next_state = self.local_esitimator.predict(next_state_index)
 
-                if group['weight_decay'] != 0:
-                    grad = grad.add(group['weight_decay'], p.data)
+        # set the target 
+        target = reward + self.discount * value_of_next_state
+        input  = self.local_esitimator.predict(current_state_index)
+        loss  = F.smooth_l1_loss(input,target)
 
-                # Decay the first and second moment running average coefficient
-                exp_avg.mul_(beta1).add_(1 - beta1, grad)
-                exp_avg_sq.mul_(beta2).addcmul_(1 - beta2, grad, grad)
+        self.global_estimator.update(loss,self.local_esitimator.model)
+    
+    def get_value_function(self):
+        return self.estimator
 
-                denom = exp_avg_sq.sqrt().add_(group['eps'])
+class PolicyModel(nn.Module):
+        def __init__(self,observation_space_size,action_space_size):
+            super().__init__()
+            self.affine = nn.Linear(observation_space_size, 128)
+            self.dropout = nn.Dropout(p=0.6)
+            self.action_head = nn.Linear(128, action_space_size)
+            
+        def forward(self, state):
+            state = torch.from_numpy(state).float()
+            state = self.affine(state)
+            state = self.dropout(state)
+            state = F.relu(state)
+            state = self.action_head(state)
+            actions_prob = F.softmax(state,dim=0)
+            return actions_prob
 
-                bias_correction1 = 1 - beta1**state['step'].item()
-                bias_correction2 = 1 - beta2**state['step'].item()
-                step_size = group['lr'] * math.sqrt(
-                    bias_correction2) / bias_correction1
+class GlobalPolicyEsitmator:
+    def __init__(self,observation_space_size,action_space_size):
+        self.model = PolicyModel(observation_space_size,action_space_size)
+        self.optimizer = SharedAdam(self.model.parameters(),lr =1e-3)
+    
+    def _sync_local_grads(local_model, global_model):
+        for local_param, global_param in zip(local_model.parameters(),global_model.parameters()):
+            if global_param.grad is not None:
+                return
+            global_param._grad = local_param.grad
 
-                p.data.addcdiv_(-step_size, exp_avg, denom)
+    def predict(self,state):
+        actions_prob=self.model.forward(state)
+        return actions_prob
 
-        return loss
+    def update(self,*args):
+        loss = args[0]
+        local_model = args[1]
+
+        self._sync_local_grads(self.model,local_model)
+        self.optimizer.zero_grad()
+        loss.backward()
+        self.optimizer.step()
+
+
+class LocalPolicyEsitmator:
+    def __init__(self,observation_space_size,action_space_size):
+        self.model = PolicyModel(observation_space_size,action_space_size)
+        
+    def predict(self,state):
+        actions_prob=self.model.forward(state)
+        return actions_prob
+
+    def update(self,*args):
+        global_model = args[0]
+        self.model.load_state_dict(global_model.state_dict())
+        
+
+class OnlineActor(ActorBase):
+    def __init__(self,global_policy_esitmator,critic,discount=1.0):
+        self.global_policy_estimator =  global_policy_esitmator
+        self.discount = discount
+        self.critic = critic
+        
+    def improve(self,*args):
+        current_state_index = args[0]
+        reward = args[1]
+        action_prob = args[2]
+        next_state_index = args[3]
+    
+        advantage = torch.tensor(reward) + self.discount* self.critic.global_estimator.predict(next_state_index) - self.critic.global_estimator.predict(current_state_index)
+        policy_loss = -torch.log(torch.round(action_prob*10**3)/10**3)*advantage.detach()
+    
+        self.global_policy_estimator.update(policy_loss)
+
+    def get_behavior_policy(self):
+        return self.policy
+
+
+class OnlineCriticActor:
+    EPS = np.finfo(np.float32).eps.item()
+    MAX_STEPS = 500000
+    def  __init__(self,critic,actor,env,num_episodes):
+        self.critic=critic 
+        self.actor= actor
+        self.env = env
+        self.num_episodes = num_episodes
+            
+    def improve(self):
+        for episode in range(0, self.num_episodes):
+            self._run_one_episode(episode)
+
+    def _run_one_episode(self, episode):
+        # S
+        current_state_index = self.env.reset()
+
+        for _ in range(0, OnlineCriticActor.MAX_STEPS):
+            # A
+            current_action_index = self.actor.get_behavior_policy().get_action(current_state_index)
+            observation = self.env.step(current_action_index)
+            self.env.render()
+
+            # R
+            reward = observation[1]
+            done = observation[2]
+
+            # S'
+            next_state_index = observation[0]
+
+            self.critic.evaluate(current_state_index,reward,next_state_index,done,episode,self.writer)
+            
+            action_prob=self.actor.get_behavior_policy().get_discrete_distribution_tensor(current_state_index)[current_action_index]
+            self.actor.improve(current_state_index,reward,action_prob,next_state_index,done,episode,self.writer)
+
+            if done:
+                break
+
+            current_state_index = next_state_index
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 class AsynchronousAdvantageActorCritic:
